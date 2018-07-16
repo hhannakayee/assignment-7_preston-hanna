6597ccd315c14b73b56e5c7a2d74ce4f3e837cf7
Hanna Preston <hkpreston92@bellsouth.net>
2018-07-16 01:10:16 -0400

    change last h2 element

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   var headings = $('h2');
 
-  $('h2').last().text('I’m Hanna Preston, and I’m learning jQuery!');
+  headings.last().text('I’m Hanna Preston, and I’m learning jQuery!');
 });
